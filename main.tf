1369e2e73943c1f0566f36752bacf75259728fe8
basil18 <raikhanzhetkerbek@gmail.com>
2024-03-15 10:32:00 -0500

    added new man.tf file

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+resource "awscc_ecr_repository" "this" {
+  repository_name      = "example-ecr"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration = {
+    scan_on_push = true
+  }
+
+}
